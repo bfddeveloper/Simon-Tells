a862a456b4d0b284851333b55b76b16e769612cd
bfddeveloper <bdicksondev@gmail.com>
2023-09-18 09:41:55 -0500

    Initial Commit

---
+++ Simon Tells/Simon_TellsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simon_TellsApp.swift
+//  Simon Tells
+//
+//  Created by Brody Dickson on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Simon_TellsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
